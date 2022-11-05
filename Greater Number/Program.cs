91ac92f736cdf65dd576b2726eb6e1a20cd5d94f
its julii <nanji828@gmail.com>
2022-11-05 20:12:42 +0200

    Add project files.

---
+++ Greater Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Greater_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numm1 = int.Parse(Console.ReadLine());
+            int numm2 = int.Parse(Console.ReadLine());
+
+            if (numm1 > numm2)
+            {
+                Console.WriteLine(numm1);
+            }
+                
+            else
+            {
+                Console.WriteLine(numm2);
+            }
+            
+        }
+    }
+}
